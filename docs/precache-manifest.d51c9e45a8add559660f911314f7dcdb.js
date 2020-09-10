46264a038cd60941dd824adfefc7a4746db2ef6a
Vagus-art <agustin.favo@gmail.com>
2020-09-10 00:11:15 -0300

    builded

---
+++ docs/precache-manifest.d51c9e45a8add559660f911314f7dcdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74b422541acda0207290bc3e43756311",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92d2b8312c4eb3e01188",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "9dd32ae14ca2c547016c",
+    "url": "/static/js/2.3518d61b.chunk.js"
+  },
+  {
+    "revision": "907a43cc49daf0b9ef1f1efa921680b3",
+    "url": "/static/js/2.3518d61b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92d2b8312c4eb3e01188",
+    "url": "/static/js/main.2b7f35ce.chunk.js"
+  },
+  {
+    "revision": "4305ad129dcf89844c80",
+    "url": "/static/js/runtime-main.3ea18b6e.js"
+  }
+]);
