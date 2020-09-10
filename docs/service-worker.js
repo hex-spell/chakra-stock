3474ab6f0b0f1193554dd09fbe5ef7eeb0f0c96f
Vagus-art <agustin.favo@gmail.com>
2020-09-10 01:19:05 -0300

    now works?

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "chakra-stock/precache-manifest.c729097b26b2dc0298547a0e5d370c6f.js"
+  "/chakra-stock/precache-manifest.6361e6cf5781bb7bfea51289b819faaa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("chakra-stock/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/chakra-stock/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
