77863d763d8633bad6e2cbad53791c1b68b6c46c
Vagus-art <agustin.favo@gmail.com>
2020-09-11 17:03:17 -0300

    tratando de arreglar router en g-pages

---
+++ docs.back/precache-manifest.c25f3f9af1582a75d0e345eae841884a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f195006b015a4fe5b00e36f7e307d1d",
+    "url": "/chakra-stock/index.html"
+  },
+  {
+    "revision": "e6a2998bef134cf17152",
+    "url": "/chakra-stock/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "2d8292a1bc07cb2d1ad8",
+    "url": "/chakra-stock/static/js/2.77b9f8b5.chunk.js"
+  },
+  {
+    "revision": "907a43cc49daf0b9ef1f1efa921680b3",
+    "url": "/chakra-stock/static/js/2.77b9f8b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6a2998bef134cf17152",
+    "url": "/chakra-stock/static/js/main.48212f4e.chunk.js"
+  },
+  {
+    "revision": "d879df82bbe95a88bd41",
+    "url": "/chakra-stock/static/js/runtime-main.c886b0e4.js"
+  },
+  {
+    "revision": "d6e9d6e28cede4789a24bac98e11a24f",
+    "url": "/chakra-stock/static/media/background.d6e9d6e2.jpeg"
+  }
+]);
